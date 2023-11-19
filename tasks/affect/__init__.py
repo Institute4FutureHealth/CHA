46b52c3abed805098eb24cc8fc68db2f7a67b496
Iman Azimi <azimi.iman.1988@gmail.com>
2023-11-19 03:29:34 -0800

    Add affect physical activity

--- tasks/affect/__init__.py
+++ tasks/affect/__init__.py
@@ -1,11 +1,15 @@
 from tasks.affect.sleep_avg import SleepAVG
-from tasks.affect.sleep_get import SleepGet
 from tasks.affect.sleep_trend import SleepTrend
+from tasks.affect.sleep_get import SleepGet
+from tasks.affect.activity_get import ActivityGet
 from tasks.affect.sleep_analysis import SleepAnalysis
+from tasks.affect.activity_analysis import ActivityAnalysis
 
 __all__ = [
     "SleepAVG",
-    "SleepGet",
     "SleepTrend",
+    "SleepGet",
+    "ActivityGet",
     "SleepAnalysis",
+    "ActivityAnalysis",
 ]
