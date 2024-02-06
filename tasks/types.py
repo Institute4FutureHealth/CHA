000298f7e5cd6db4f61eb1c2f4c120f461b0e1f0
mahyar12 <mahyar12@Mahyars-Mac-Studio.local>
2024-02-06 13:09:46 -0800

    removed deprecated tasks

--- tasks/types.py
+++ tasks/types.py
@@ -15,14 +15,7 @@ from tasks.nutritionix.calculate_food_risk_factor import (
     CalculateFoodRiskFactor,
 )
 from tasks.nutritionix.query_nutritionix import QueryNutritionix
-from tasks.playwright import Click
-from tasks.playwright import CurrentWebPage
-from tasks.playwright import ExtractHyperlinks
 from tasks.playwright import ExtractText
-from tasks.playwright import GetElements
-from tasks.playwright import Navigate
-from tasks.playwright import NavigateBack
-from tasks.read_from_datapipe import ReadDataPipe
 from tasks.run_python_code import RunPythonCode
 from tasks.serpapi import SerpAPI
 from tasks.task import BaseTask
@@ -32,13 +25,7 @@ from tasks.test_file import TestFile
 
 TASK_TO_CLASS: Dict[TaskType, Type[BaseTask]] = {
     TaskType.SERPAPI: SerpAPI,
-    TaskType.CLICK: Click,
-    TaskType.GET_CURRENT_PAGE: CurrentWebPage,
-    TaskType.EXTRACT_HYPERLINKS: ExtractHyperlinks,
     TaskType.EXTRACT_TEXT: ExtractText,
-    TaskType.GET_ELEMENTS: GetElements,
-    TaskType.NAVIGATE_BACK: NavigateBack,
-    TaskType.NAVIGATE: Navigate,
     TaskType.AFFECT_SLEEP_GET: SleepGet,
     TaskType.AFFECT_ACTIVITY_GET: ActivityGet,
     TaskType.AFFECT_SLEEP_ANALYSIS: SleepAnalysis,
@@ -46,7 +33,6 @@ TASK_TO_CLASS: Dict[TaskType, Type[BaseTask]] = {
     TaskType.GOOGLE_TRANSLATE: GoogleTranslate,
     TaskType.ASK_USER: AskUser,
     TaskType.TEST_FILE: TestFile,
-    TaskType.READ_FROM_DATAPIPE: ReadDataPipe,
     TaskType.RUN_PYTHON_CODE: RunPythonCode,
     TaskType.PPG_GET: PPGGet,
     TaskType.PPG_ANALYSIS: PPGAnalysis,
