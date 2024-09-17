2c4eb0bf4d2bde062e977d088e73846571b34461
Mahyar12 <mahyar12.abbasian@gmail.com>
2024-09-17 13:14:58 -0700

    Update base.py

--- interface/base.py
+++ interface/base.py
@@ -15,8 +15,10 @@ class Interface(BaseModel):
     def validate_environment(cls, values: Dict) -> Dict:
         """
         Validate that api key and python package exists in environment.
+        
         This function checks if the `gradio` Python package is installed in the environment. If the package is not found, it raises a `ValueError`
         with an appropriate error message.
+        
         Args:
             cls (object): The class to which this method belongs.
             values (Dict): A dictionary containing the environment values.
@@ -24,6 +26,8 @@ class Interface(BaseModel):
             Dict: The updated `values` dictionary with the `gradio` package imported.
         Raise:
             ValueError: If the `gradio` package is not found in the environment.
+
+            
         Example:
             .. code-block:: python
                 from langchain import ReActChain, OpenAI
@@ -57,10 +61,12 @@ class Interface(BaseModel):
     ):
         """
         Prepare the Gradio interface for the chatbot.
+        
         This method sets up the Gradio interface for the chatbot.
         It creates various UI components such as a textbox for user input, a checkbox for enabling/disabling chat history,
         a dropdown for selecting tasks, and a clear button to reset the interface. The interface is then launched and stored
         in the `self.interface` attribute.
+        
         Args:
             self (object): The instance of the class.
             respond (function): The function to handle user input and generate responses.
@@ -70,6 +76,8 @@ class Interface(BaseModel):
             share (bool, optional): Flag indicating whether to enable sharing the interface. Defaults to False.
         Return:
             None
+
+            
         Example:
             .. code-block:: python
                 from langchain import ReActChain, OpenAI
@@ -153,12 +161,17 @@ class Interface(BaseModel):
     def close(self):
         """
         Close the Gradio interface.
+        
         This method closes the Gradio interface associated with the chatbot.
         It calls the `close` method of the interface object stored in the `self.interface` attribute.
+        
         Args:
             self (object): The instance of the class.
         Return:
             None
+
+
+            
         Example:
             .. code-block:: python
                 from langchain import ReActChain, OpenAI
